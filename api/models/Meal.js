adc51180bbfa4f70e2a3464073eceb6ce2c4cb86
Stuart Carruthers <stu@ironiclensflare.com>
2017-04-01 16:51:02 +0100

    MongoDB

---
+++ api/models/Meal.js
@@ -0,0 +1,19 @@
+/**
+ * Meal.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  schema: true,
+  attributes: {
+    name: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+    date: 'date',
+    required: true
+  }
+};
